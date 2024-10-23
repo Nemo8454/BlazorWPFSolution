5611ca095a769f0e3367595cc819a9d57a1e78b7
Nemo <845412244@qq.com>
2024-10-23 13:34:08 +0800

    添加项目文件。

---
+++ BlazorWPF/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using static BlazorWPF.Startup;
+
+namespace BlazorWPF;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        Init();
+        InitializeComponent();
+    }
+}
